0ca8a8017365d63f41cea4b6574f6f4824836198
bilet-13 <r10922114@csie.ntu.edu.tw>
2026-03-23 22:30:56 -0700

    fix(locust): Fix to remain single source of truth

--- locust_test/check_result_after_locust.sh
+++ locust_test/check_result_after_locust.sh
@@ -1,14 +1,17 @@
 source "$(dirname "$0")/locust_config.sh"
+PROJECT_ROOT="$(dirname "$0")/.."
+set -a
+source "$PROJECT_ROOT/.env"
+set +a
 
-echo "check number  of orders placed by locust users..."
-docker exec -it flash_sale_postgres psql -U flash_user -d flash_sale_db \
+echo "check number of orders placed by locust users..."
+docker exec -it flash_sale_postgres psql -U $POSTGRES_USER -d $POSTGRES_DB \
   -c "SELECT COUNT(*) FROM orders WHERE user_id IN (SELECT id FROM users WHERE username LIKE 'locust_%');"
 
-
 echo "check remaining stock for the target product..."
-docker exec -it flash_sale_postgres psql -U flash_user -d flash_sale_db \
+docker exec -it flash_sale_postgres psql -U $POSTGRES_USER -d $POSTGRES_DB \
   -c "SELECT stock FROM products WHERE id = $TARGET_PRODUCT_ID;"
 
 echo "check remaining stock in Redis for the target product..."
-docker exec -it flash_sale_redis redis-cli -a redis_secure_pass_2024 \
+docker exec -it flash_sale_redis redis-cli -a $REDIS_PASSWORD \
   GET stock:product:$TARGET_PRODUCT_ID
--- locust_test/locust.sh
+++ locust_test/locust.sh
@@ -1,15 +1,19 @@
 source "$(dirname "$0")/locust_config.sh"
+PROJECT_ROOT="$(dirname "$0")/.."
+set -a
+source "$PROJECT_ROOT/.env"
+set +a
 
 # clean data
-docker exec -it flash_sale_postgres psql -U flash_user -d flash_sale_db \
+docker exec -it flash_sale_postgres psql -U $POSTGRES_USER -d $POSTGRES_DB \
   -c "DELETE FROM orders WHERE user_id IN (SELECT id FROM users WHERE username LIKE 'locust_%');"
-docker exec -it flash_sale_postgres psql -U flash_user -d flash_sale_db \
+docker exec -it flash_sale_postgres psql -U $POSTGRES_USER -d $POSTGRES_DB \
   -c "DELETE FROM users WHERE username LIKE 'locust_%';"
 
 # set stock in both Postgres and Redis
-docker exec -it flash_sale_postgres psql -U flash_user -d flash_sale_db \
+docker exec -it flash_sale_postgres psql -U $POSTGRES_USER -d $POSTGRES_DB \
   -c "UPDATE products SET stock = $INITIAL_STOCK WHERE id = $TARGET_PRODUCT_ID;"
-docker exec -it flash_sale_redis redis-cli -a redis_secure_pass_2024 \
+docker exec -it flash_sale_redis redis-cli -a $REDIS_PASSWORD \
   SET stock:product:$TARGET_PRODUCT_ID $INITIAL_STOCK
 
 export TARGET_PRODUCT_ID
